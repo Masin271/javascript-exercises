7d46c1d99981c4d6609b63d6018795a46fcaf30a
Masin271 <masinmasin271@gmail.com>
2024-10-01 18:34:46 +0330

    Look at my masterpiece!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
-
+function removeFromArray(arr, ...arg) {
+            for (let i = arr.length-1; i >= 0 ;i--) {
+                for (const j of arg) {
+                    if (arr.indexOf(j) > -1 && arr[i] == j) {
+                    arr.splice(arr.indexOf(j), 1);
+                    i++;
+                    }
+                }
+                    
+    }
+    return arr;
+    }
+    
 // Do not edit below this line
 module.exports = removeFromArray;
